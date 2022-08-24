4b413d8449def33dfb19ede84bd35fcb13da6102
pl4sma2389 <ohyeah2389@yahoo.com>
2022-08-24 07:14:24 -0400

    First Dear PyGui version

--- main.py
+++ main.py
@@ -1,58 +1,89 @@
-import tkinter
-import tkinter.font
-import customtkinter
+import dearpygui.dearpygui as dpg
+import webcolors as wc
 import configparser
-from pyglet import font as pyglet_font
 import os
 
 
 # Settings, later will be loadable from .ini file via ConfigParser
+BACK_COLOR = "#232323"
+BACK_COLOR_LIGHT = "#232323"
 ACCENT_COLOR = "#cf1717"
 ACCENT_COLOR_DARK = "#8a0f0f"
 WINDOW_SIZE_X = 1200
 WINDOW_SIZE_Y = 700
-UI_PADDING = 10
-UI_CORNER_RADIUS = 5
-UI_TAB_HEIGHT = 40
+UI_CORNER_RADIUS = 3
+UI_FONT_SIZE_REGULAR = 20
+UI_FONT_SIZE_LARGE = 25
+UI_FONT_SIZE_HEADING = 30
+UI_FONT_SIZE_TITLE = 40
 
 
-# Set up CustomTkinter
-customtkinter.set_appearance_mode("dark")
+dpg.create_context()
+dpg.create_viewport(title='KRPManager', width=WINDOW_SIZE_X, height=WINDOW_SIZE_Y, min_width=700, min_height=500)
+dpg.set_viewport_vsync(True)
 
-# Initialize the window
-root_tk = customtkinter.CTk()
-root_tk.geometry("{x_size}x{y_size}".format(x_size=WINDOW_SIZE_X, y_size=WINDOW_SIZE_Y))
-root_tk.title("Kart Racing Pro Manager")
-'''root_tk.iconbitmap("Graphics\\krpmanager_icon.ico")'''
-root_tk.resizable(False, False)
-root_tk.configure(fg_color="#232323")
 
-# Set the font
-pyglet_font.add_directory(os.path.join(os.path.dirname(os.path.abspath(__file__)), "Graphics\\Fonts"))
-large_font = tkinter.font.Font(family="serpentine_sans_icg", size=30)
-small_font = tkinter.font.Font(family="serpentine_sans_icg", size=12)
+# Register fonts and their sizes
+with dpg.font_registry():
+    regular_font = dpg.add_font("Graphics\\Fonts\\RobotoMono-SemiBold.ttf", UI_FONT_SIZE_REGULAR)
+    large_font = dpg.add_font("Graphics\\Fonts\\RobotoMono-SemiBold.ttf", UI_FONT_SIZE_LARGE)
+    heading_font = dpg.add_font("Graphics\\Fonts\\Serpentine Sans ICG Bold.ttf", UI_FONT_SIZE_HEADING)
+    title_font = dpg.add_font("Graphics\\Fonts\\Serpentine Sans ICG Bold.ttf", UI_FONT_SIZE_TITLE)
 
-# Initialize tab selector frame and the different selected tab frames
-tab_frame_tk = customtkinter.CTkFrame(master=root_tk, width=WINDOW_SIZE_X-(UI_PADDING*2), height=UI_TAB_HEIGHT, corner_radius=UI_CORNER_RADIUS)
-settings_frame_tk = customtkinter.CTkFrame(master=root_tk, width=WINDOW_SIZE_X-(UI_PADDING*2), height=WINDOW_SIZE_Y-UI_TAB_HEIGHT-(UI_PADDING*3), corner_radius=UI_CORNER_RADIUS)
 
-# Place them correctly
-tab_frame_tk.place(x=UI_PADDING, y=UI_PADDING, width=WINDOW_SIZE_X-(UI_PADDING*2), height=UI_TAB_HEIGHT)
-settings_frame_tk.place(x=UI_PADDING, y=60, width=WINDOW_SIZE_X-(UI_PADDING*2), height=WINDOW_SIZE_Y-UI_TAB_HEIGHT-(UI_PADDING*3))
+# Set up theme and colors
+with dpg.theme() as global_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_WindowBg, wc.hex_to_rgb(BACK_COLOR), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_MenuBarBg, wc.hex_to_rgb(BACK_COLOR_LIGHT), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_TabActive, wc.hex_to_rgb(ACCENT_COLOR), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_TabHovered, wc.hex_to_rgb(ACCENT_COLOR_DARK), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, UI_CORNER_RADIUS, category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_TabRounding, UI_CORNER_RADIUS, category=dpg.mvThemeCat_Core)
 
-# Change their colors
-tab_frame_tk.configure(fg_color="#404040")
-settings_frame_tk.configure(fg_color="#404040")
 
-# Spawn the tabs
-tab_button_settings = customtkinter.CTkButton(master=tab_frame_tk, text="Settings", width=90, height=30, text_font=small_font, fg_color=ACCENT_COLOR, hover_color=ACCENT_COLOR_DARK)
+with dpg.window(tag="Main Window"):
+    with dpg.tab_bar():
+        with dpg.tab(label="Manage"):
+            with dpg.tab_bar():
+                with dpg.tab(label="Tracks"):
+                    window_heading = dpg.add_text("Track Management")
+                    dpg.add_text("Track Management will go here")
+                    dpg.bind_font(regular_font)
+                    dpg.bind_item_font(window_heading, heading_font)
 
-# List them for quick organization
-tabs_list = [tab_button_settings]
+                with dpg.tab(label="Karts"):
+                    window_heading = dpg.add_text("Karts Management")
+                    dpg.add_text("Karts Management will go here")
+                    dpg.bind_font(regular_font)
+                    dpg.bind_item_font(window_heading, heading_font)
 
-# Organize the tabs
-for index_number, tab_name in enumerate(tabs_list):
-    tab_name.grid(column=index_number, row=0, pady=5, padx=5)
+                with dpg.tab(label="Skins"):
+                    window_heading = dpg.add_text("Skins Management")
+                    dpg.add_text("Skins Management will go here")
+                    dpg.bind_font(regular_font)
+                    dpg.bind_item_font(window_heading, heading_font)
 
-# Start the GUI
-root_tk.mainloop()
+        with dpg.tab(label="Settings"):
+            dpg.add_text("Settings will go here")
+
+        with dpg.tab(label="About"):
+            window_title = dpg.add_text("KRPManager")
+            dpg.add_text("Developed by pl4sma2389 at Slip Angle Modding and Development\n\nThe following software and libraries are used in this program:\n\nPython 3.10\n\tDear PyGui\n\twebcolors 1.3")
+            dpg.bind_font(regular_font)
+            dpg.bind_item_font(window_title, title_font)
+
+    dpg.bind_font(regular_font)
+
+
+dpg.bind_theme(global_theme)
+# dpg.show_style_editor()
+# dpg.show_imgui_demo()
+dpg.setup_dearpygui()
+dpg.show_viewport()
+
+dpg.set_primary_window("Main Window", True)
+
+dpg.start_dearpygui()
+
+dpg.destroy_context()
